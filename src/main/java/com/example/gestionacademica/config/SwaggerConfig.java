4c790ceff2de75f091707c7f6590b80643e0b184
TicseTorresArley <u23238849@utp.edu.pe>
2026-04-23 07:30:28 -0500

    fix(swagger): usar servidor relativo y forwarded headers

--- src/main/java/com/example/gestionacademica/config/SwaggerConfig.java
+++ src/main/java/com/example/gestionacademica/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,6 +16,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/")))
                 .info(new Info()
                         .title("Sistema de Gestión Académica Universitaria")
                         .description("""
